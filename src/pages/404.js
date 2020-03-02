37ca4f3457460ace1070969f51f5de16afd199c1
Dennis Xiao <dennis.xiao@autogeneral.com.au>
2020-03-02 15:05:27 +0800

    add 404 page style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout/Layout"
+import Banner from "../components/Banner/Banner"
+import { Link } from "gatsby"
+import styles from "./404.module.scss"
 
 class ErrorPage extends Component {
   render() {
     return (
       <Layout>
-        Error page
+        <header className={styles.error}>
+          <Banner title="您要找到页面消失啦～">
+            <Link to="/" className="btn-white">
+              回到首页 >>
+            </Link>
+          </Banner>
+        </header>
       </Layout>
     )
   }
