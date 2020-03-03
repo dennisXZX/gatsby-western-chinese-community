406848bd0bb96f4c0f405c29b1c08c759b222a96
Dennis Xiao <dennis.xiao@autogeneral.com.au>
2020-03-03 16:49:42 +0800

    add gatsby-source-filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,20 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "西区华人之家",
+    description: "布里斯班西区华人之家",
+    author: "Dennis Xiao",
+  },
   plugins: [
-    'gatsby-plugin-sass',
-    'gatsby-plugin-styled-components'
-  ]
+    "gatsby-plugin-sass",
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images/`,
+      }
+    }
+  ],
 }
